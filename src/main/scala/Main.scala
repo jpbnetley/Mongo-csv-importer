83a0519b26bab5ba42c4e780913d21ac308913aa
Jonathan Netley <jnetley@fullfacing.com>
2019-07-04 22:55:03 +0200

    read csv files and insert into mongo

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,25 @@
 import Util.Database.Database
+import Util.File.FileHelper
 
 object Main extends App {
 
-  val db = Database.init()
+  val db        = Database.init()
+  val pathToCsv = "/Users/jonathan/Downloads/northwind-mongo-master"
+  val csvFiles  = FileHelper.getCsvFiles(pathToCsv)
+
+  for ((file, index) <- csvFiles.zipWithIndex) {
+    println(s"Processing file ${index + 1} of ${csvFiles.length}")
+
+    val fileLines = FileHelper.extractCsvFileLines(file)
+    val headers   = fileLines.headOption.map(_.split(',').toList)
+    val body      = fileLines.drop(1)
+    val collectionName = file.getName.replace(".csv","").toLowerCase
+
+    db.createCollection(collectionName)
+    db.getCollection(collectionName).bulkWrite(body)
+    println(s"Done processing file ${index + 1}")
+
+  }
 
 
 }
--- src/main/scala/Util/File/FileHelper.scala
+++ src/main/scala/Util/File/FileHelper.scala
@@ -1,5 +1,25 @@
 package Util.File
+import java.io.File
+import java.nio.charset.StandardCharsets
+
+import scala.io.Source
 
 object FileHelper {
+
+  def getListOfFiles(dir: File, extensions: List[String]): List[File] = {
+    dir.listFiles.filter(_.isFile).toList.filter { file =>
+      extensions.exists(file.getName.endsWith(_))
+    }
+  }
+
+  def getCsvFiles(dir: String): List[File] = {
+    val okFileExtensions = List("csv")
+    getListOfFiles(new File(dir), okFileExtensions)
+  }
+
+  def extractCsvFileLines(file: File): List[String] = {
+    val reader = Source.fromFile(file.toPath.toUri, StandardCharsets.ISO_8859_1.name())
+    reader.getLines().toList
+  }
 
 }
