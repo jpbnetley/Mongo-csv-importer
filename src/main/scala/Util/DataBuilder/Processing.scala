b6adcc433e8738ad93effa4e30b9cda37f58a952
Jonathan Netley <jpbnetley@gmail.com>
2020-04-19 22:18:44 +0200

    database cleanup

--- src/main/scala/Util/DataBuilder/Processing.scala
+++ src/main/scala/Util/DataBuilder/Processing.scala
@@ -10,8 +10,6 @@ import org.bson.Document
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
-import scala.reflect.io.File
-import Util.Logging.log
 import Util.ErrorHandler._
 import Util.Models.OrderedFile
 
@@ -25,22 +23,26 @@ object Processing {
     */
   def csvFiles(csvFiles: List[OrderedFile])
               (implicit database: Database.type): Task[Either[Exception, Unit]] = {
-    Task.wander(csvFiles) { orderedFile =>
-      println(s"Processing file ${orderedFile.index + 1} of ${csvFiles.length} file name: ${orderedFile.file.name}")
-      (for {
-        fileLines       <- EitherT(FileHelper.extractCsvFileLines(orderedFile.file))
-        headers         =  fileLines.headOption.map(_.split(',').toList)
-        lineItems       =  fileLines.drop(1)
-        collectionName  =  orderedFile.file.name.replace(".csv", "").toLowerCase
-        documentResult  <- EitherT(buildMongoDocuments(headers, lineItems))
-        db              <- EitherT.right[Exception](database.getDatabase)
-        dbInsert        <- EitherT.rightT[Task, Exception](db.getCollection[Document](collectionName).insertMany(documentResult))
-      } yield {
-        println(s"Inserting into db: $dbInsert")
-        Await.result(dbInsert.toFuture(), Duration.Inf)
-        println(s"Done processing file ${orderedFile.index + 1}")
-      }).value
-    }.map { result =>
+    val insertion = {
+      Task.wander(csvFiles) { orderedFile =>
+        println(s"Processing file ${orderedFile.index + 1} of ${csvFiles.length} file name: ${orderedFile.file.name}")
+        (for {
+          fileLines <- EitherT(FileHelper.extractCsvFileLines(orderedFile.file))
+          headers = fileLines.headOption.map(_.split(',').toList)
+          lineItems = fileLines.drop(1)
+          collectionName = orderedFile.file.name.replace(".csv", "").toLowerCase
+          documentResult <- EitherT(buildMongoDocuments(headers, lineItems))
+          db <- EitherT(database.getDatabase)
+          dbInsert <- EitherT.rightT[Task, Exception](db.getCollection[Document](collectionName).insertMany(documentResult))
+        } yield {
+          println(s"Inserting into db: $dbInsert")
+          Await.result(dbInsert.toFuture(), Duration.Inf)
+          println(s"Done processing file ${orderedFile.index + 1}")
+        }).value
+      }
+    }
+
+    insertion.map { result =>
       val (errors, _) = result.separate
       errors.headOption.map(error).toLeft(())
     }
